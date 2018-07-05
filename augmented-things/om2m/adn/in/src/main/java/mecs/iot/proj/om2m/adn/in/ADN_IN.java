a5ab55e143be2a2abce3918a740cb5d65a07520e
openformatproj <a.trifoglio@gmail.com>
2018-07-05 13:24:22 +0200

    Test

--- augmented-things/om2m/adn/in/src/main/java/mecs/iot/proj/om2m/adn/in/ADN_IN.java
+++ augmented-things/om2m/adn/in/src/main/java/mecs/iot/proj/om2m/adn/in/ADN_IN.java
@@ -183,18 +183,19 @@ class ADN_IN extends ADN {
 			if (notification.contains("m2m:vrq")) {
 				outStream.out1("Handling subscription confirmation", i);
 			} else {
-				String con = null;
-				try {
-					con = Services.parseJSON(notification, new String[] {"m2m:sgn","m2m:nev","m2m:rep","m2m:cin"},
-							new String[] {"con"}, new Class<?>[] {String.class},false);
-				} catch (JSONException e) {
-					debugStream.out("Received invalid notification", i);
-					response = new Response(ResponseCode.BAD_REQUEST);
-					exchange.respond(response);
-					i++;
-					return;
-				}
-				outStream.out1("Handling notification with JSON: " + con, i);
+//				String con = null;
+//				try {
+//					con = Services.parseJSON(notification, new String[] {"m2m:sgn","m2m:nev","m2m:rep","m2m:cin"},
+//							new String[] {"con"}, new Class<?>[] {String.class},false);
+//				} catch (JSONException e) {
+//					debugStream.out("Received invalid notification", i);
+//					response = new Response(ResponseCode.BAD_REQUEST);
+//					exchange.respond(response);
+//					i++;
+//					return;
+//				}
+//				outStream.out1("Handling notification with JSON: " + con, i);
+				outStream.out1("Received (test): " + notification, i);
 			}
 			response = new Response(ResponseCode.CREATED);
 		}
