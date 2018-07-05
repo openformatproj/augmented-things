39ee6c282c7466044d645dc95e360443e13148f0
openformatproj <a.trifoglio@gmail.com>
2018-07-05 13:03:27 +0200

    Handle notifications

--- augmented-things/om2m/adn/in/src/main/java/mecs/iot/proj/om2m/adn/in/ADN_IN.java
+++ augmented-things/om2m/adn/in/src/main/java/mecs/iot/proj/om2m/adn/in/ADN_IN.java
@@ -4,6 +4,7 @@ import java.net.URISyntaxException;
 import java.util.HashMap;
 
 import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.coap.Response;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 
@@ -88,88 +89,98 @@ class ADN_IN extends ADN {
 	synchronized public void handlePOST(CoapExchange exchange) {
 		Response response = null;
 		String id = getUriValue(exchange,"id",0);
-		if (id==null || !isValidId(id)) {
-			if (id!=null)
+		if (id!=null) {
+			if (!isValidId(id)) {
 				debugStream.out("Bad request, id=" + id, i);
-			else
-				debugStream.out("Bad request, id", i);
-			response = new Response(ResponseCode.BAD_REQUEST);
-			exchange.respond(response);
-			i++;
-			return;
-		}
-		String serial = getUriValue(exchange,"ser",1);
-		if (serial!=null) {
-			// node IN registration and localization (id=<ID>&ser=<SERIAL>&loc=<LOC>)
-			if (!isValidSerial(serial)) {
-				debugStream.out("Bad request, ser=" + serial, i);
 				response = new Response(ResponseCode.BAD_REQUEST);
 				exchange.respond(response);
 				i++;
 				return;
 			}
-			String location = getUriValue(exchange,"loc",2);
-			if (location==null || !isValidLocation(location)) {
-				if (location!=null)
-					debugStream.out("Bad request, loc=" + location, i);
-				else
-					debugStream.out("Bad request, loc", i);
-				response = new Response(ResponseCode.BAD_REQUEST);
-				exchange.respond(response);
-				i++;
-				return;
+			String serial = getUriValue(exchange,"ser",1);
+			if (serial!=null) {
+				// node IN registration and localization (id=<ID>&ser=<SERIAL>&loc=<LOC>)
+				if (!isValidSerial(serial)) {
+					debugStream.out("Bad request, ser=" + serial, i);
+					response = new Response(ResponseCode.BAD_REQUEST);
+					exchange.respond(response);
+					i++;
+					return;
+				}
+				String location = getUriValue(exchange,"loc",2);
+				if (location==null || !isValidLocation(location)) {
+					if (location!=null)
+						debugStream.out("Bad request, loc=" + location, i);
+					else
+						debugStream.out("Bad request, loc", i);
+					response = new Response(ResponseCode.BAD_REQUEST);
+					exchange.respond(response);
+					i++;
+					return;
+				}
+				MN mn = Db.mnMap.get(Integer.parseInt(location));
+				if (mn==null || !mn.active) {
+					debugStream.out("MN with location \"" + location + "\" is not registered", i);
+					response = new Response(ResponseCode.BAD_REQUEST);
+					exchange.respond(response);
+					i++;
+					return;
+				}
+				outStream.out1("Associating node \"" + id + "\" with serial \"" + serial + "\" to MN \"" + mn.id + "\"", i);
+				mnMap.put(serial,mn);
+				response = new Response(ResponseCode.CREATED);
+				response.setPayload(mn.id + ", " + mn.address);
+			} else {
+				// MN registration (id=<ID>)
+				MN[] mns = Db.mnMap.values().toArray(new MN[] {});
+				int index = 0;
+				boolean found = false;
+				for (int i=0; i<mns.length; i++) {
+					if (id.equals(mns[i].id)) {
+						index = i;
+						found = true;
+						break;
+					}	
+				}
+				if (!found) {
+					debugStream.out("MN with id \"" + id + "\" doesn't exist", i);
+					response = new Response(ResponseCode.BAD_REQUEST);
+					exchange.respond(response);
+					i++;
+					return;
+				}
+				outStream.out1("Registering MN \"" + id + "\"", i);
+				mns[index].active = true;
+				cseClient.stepCount();
+				try {
+					cseClient.connect(Constants.cseProtocol + mns[index].address + Constants.mnCSERoot(mns[index].id));
+				} catch (URISyntaxException e) {
+					outStream.out2("failed");
+					errStream.out(e, i, Severity.MEDIUM);
+					return;
+				}
+				String[] uri = new String[] {mns[index].id,"state"};
+				try {
+					cseClient.services.postSubscription(Constants.adnProtocol+"localhost"+Constants.inADNRoot,"subscription",uri,cseClient.getCount());
+				} catch (URISyntaxException e) {
+					outStream.out2("failed");
+					errStream.out(e,i,Severity.MEDIUM);
+					response = new Response(ResponseCode.INTERNAL_SERVER_ERROR);
+					exchange.respond(response);
+					i++;
+					return;
+				}
+				response = new Response(ResponseCode.CREATED);
 			}
-			MN mn = Db.mnMap.get(Integer.parseInt(location));
-			if (mn==null || !mn.active) {
-				debugStream.out("MN with location \"" + location + "\" is not registered", i);
-				response = new Response(ResponseCode.BAD_REQUEST);
-				exchange.respond(response);
-				i++;
-				return;
-			}
-			outStream.out1("Associating node \"" + id + "\" with serial \"" + serial + "\" to MN \"" + mn.id + "\"", i);
-			mnMap.put(serial,mn);
-			response = new Response(ResponseCode.CREATED);
-			response.setPayload(mn.id + ", " + mn.address);
 		} else {
-			// MN registration (id=<ID>)
-			MN[] mns = Db.mnMap.values().toArray(new MN[] {});
-			int index = 0;
-			boolean found = false;
-			for (int i=0; i<mns.length; i++) {
-				if (id.equals(mns[i].id)) {
-					index = i;
-					found = true;
-					break;
-				}	
-			}
-			if (!found) {
-				debugStream.out("MN with id \"" + id + "\" doesn't exist", i);
-				response = new Response(ResponseCode.BAD_REQUEST);
-				exchange.respond(response);
-				i++;
-				return;
-			}
-			outStream.out1("Registering MN \"" + id + "\"", i);
-			mns[index].active = true;
-			cseClient.stepCount();
-			try {
-				cseClient.connect(Constants.cseProtocol + mns[index].address + Constants.mnCSERoot(mns[index].id));
-			} catch (URISyntaxException e) {
-				outStream.out2("failed");
-				errStream.out(e, i, Severity.MEDIUM);
-				return;
-			}
-			String[] uri = new String[] {mns[index].id,"state"};
-			try {
-				cseClient.services.postSubscription(Constants.adnProtocol+"localhost"+Constants.inADNRoot,"subscription",uri,cseClient.getCount());
-			} catch (URISyntaxException e) {
-				outStream.out2("failed");
-				errStream.out(e,i,Severity.MEDIUM);
-				response = new Response(ResponseCode.INTERNAL_SERVER_ERROR);
-				exchange.respond(response);
-				i++;
-				return;
+			// notifications from subscriptions
+			String notification = exchange.getRequestText();
+			if (notification.contains("m2m:vrq")) {
+				outStream.out1("Handling subscription confirmation", i);
+			} else {
+				String con = Services.parseJSON(notification, new String[] {"m2m:sgn","m2m:nev","m2m:rep","m2m:cin"},
+						new String[] {"con"}, new Class<?>[] {String.class},false);
+				outStream.out1("Handling notification with JSON: " + con, i);
 			}
 			response = new Response(ResponseCode.CREATED);
 		}
