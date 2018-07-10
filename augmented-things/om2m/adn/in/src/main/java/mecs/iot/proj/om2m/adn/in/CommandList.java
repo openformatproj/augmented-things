885475029a304e643020e46304684da65c695f3f
openformatproj <a.trifoglio@gmail.com>
2018-07-10 22:40:01 +0200

    Minor edits

--- augmented-things/om2m/adn/in/src/main/java/mecs/iot/proj/om2m/adn/in/CommandList.java
+++ augmented-things/om2m/adn/in/src/main/java/mecs/iot/proj/om2m/adn/in/CommandList.java
@@ -14,10 +14,10 @@ class CommandList extends mecs.iot.proj.om2m.dashboard.CommandList {
 		commands[1] = (options) -> adn.cloud.getJSONNodes(options[0]);
 		commands[2] = (options) -> adn.cloud.getJSONUsers(options[0]);
 		commands[3] = (options) -> adn.cloud.getJSONSubscriptions(options[0],options[1]);
-		text[0] = new String[] {"mns","Query for the active middle-nodes","mns"};
-		text[1] = new String[] {"nodes","Query for the nodes attached to a mn","nodes -<MN>"};
-		text[2] = new String[] {"users","Query for the users attached to a mn","users -<MN>"};
-		text[3] = new String[] {"subs","Query for the subscriptions active for a given sensor","subs -<MN> -<ID>"};
+		text[0] = new String[] {"mns","Query the active mn","mns"};
+		text[1] = new String[] {"nodes","Query the nodes attached to a mn","nodes -<MN>"};
+		text[2] = new String[] {"users","Query the users attached to a mn","users -<MN>"};
+		text[3] = new String[] {"subs","Query the subscriptions active for a given resource","subs -<MN> -<ID>"};
 	}
 
 }
